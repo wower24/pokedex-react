e69b045117ed93434d8c7a73917cc65d125495aa
Marta Wower <marta.wower24@gmail.com>
2025-09-27 10:05:39 -0600

    Configuration Done

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,9 +1,15 @@
+import { Header } from "./components/Header"
+import { PokeCard } from "./components/PokeCard"
+import { SideNav } from "./components/SideNav"
+
 
 function App() {
 
   return (
     <>
-    
+      <Header />
+      <SideNav />
+      <PokeCard />
     </>
   )
 }
