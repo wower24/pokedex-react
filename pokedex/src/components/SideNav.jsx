bbe6b4e1c324614b9b981109ae2d6ad66f593d04
Marta Wower <marta.wower24@gmail.com>
2025-09-27 15:16:31 -0600

    Side Nav set up

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,13 +1,19 @@
+import { getFullPokedexNumber } from "../utils"
 import { first151Pokemon } from "../utils"
 
 export function SideNav() {
     return (
         <nav>
+            <div className={'header'}>
+                <h1 className='text-gradient'>Pokédex</h1>
+            </div>
+            <input />
             {
                 first151Pokemon.map((pokemon, pokemonIndex) => {
                     return(
-                        <button>
-
+                        <button className={'nav-card'}>
+                            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                            <p>{pokemon}</p>
                         </button>
                     )
                 })
