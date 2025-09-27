da8d7eb0185c49b47c7de7d969a88fbc2ade8c28
Marta Wower <marta.wower24@gmail.com>
2025-09-27 10:12:31 -0600

    Update SideNav.jsx

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,7 +1,17 @@
+import { first151Pokemon } from "../utils"
+
 export function SideNav() {
     return (
         <nav>
-            
+            {
+                first151Pokemon.map((pokemon, pokemonIndex) => {
+                    return(
+                        <button>
+
+                        </button>
+                    )
+                })
+            }
         </nav>
     )
 }
