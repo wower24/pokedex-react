fd2fcdc1697129bd10f6f67553ff573bf66f7f67
Marta Wower <marta.wower24@gmail.com>
2025-09-30 13:28:34 -0600

    Search functionality
    Completed input so the user can search Pokemon based on name and number

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,25 +1,48 @@
 import { getFullPokedexNumber } from "../utils"
 import { first151Pokemon } from "../utils"
+import { useState } from 'react'
 
 export default function SideNav(props) {
     const {selectedPokemon, setSelectedPokemon} = props
+
+    const [searchPhrase, setSearchPhrase] = useState('')
+
+    const filteredPokemon = first151Pokemon.filter((element, elementIndex) => {
+        if(toString(getFullPokedexNumber(elementIndex)).includes(searchPhrase)) {
+                return true
+        }
+
+        if(element.toLowerCase().includes(searchPhrase.toLowerCase())) {
+            return true
+        }
+
+        else
+            return false
+    })
 
     return (
         <nav>
             <div className={'header'}>
                 <h1 className='text-gradient'>Pokédex</h1>
             </div>
-            <input />
+            <input 
+                value={searchPhrase}
+                onChange={ (e) =>
+                    setSearchPhrase(e.target.value)
+                }
+            />
             {
-                first151Pokemon.map((pokemon, pokemonIndex) => {
+                filteredPokemon.map((pokemon, pokemonIndex) => {
                     return(
                         <button 
                         key={pokemonIndex}
-                        className={'nav-card'}
+                        className={
+                            'nav-card' + (pokemonIndex === selectedPokemon ? 'nav-card-selected' : '')
+                        }
                         onClick={()=>{
-                            setSelectedPokemon(pokemonIndex)
+                            setSelectedPokemon(first151Pokemon.indexOf(pokemon))
                         }}>
-                            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                            <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
                             <p>{pokemon}</p>
                         </button>
                     )
